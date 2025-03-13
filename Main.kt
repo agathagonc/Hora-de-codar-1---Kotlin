4a4c7be45b46f55b2004b1f5a15a49e1d749646e
Agatha Gonçalves <agathagoncalves002@gmail.com>
2025-03-13 10:13:32 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(){
+
+    var nome_do_carro = "Fusca"
+
+    print("O nome do carro é:" +
+            "$nome_do_carro")
+
+
+
+}
+
+
+
