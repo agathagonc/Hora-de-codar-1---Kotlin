bfe531007e3975bf5d6a94e70fbc2536cb61413d
Agatha Gonçalves <agathagoncalves002@gmail.com>
2025-03-17 20:23:22 -0300

    Add files via upload

---
+++ 2.kt
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    //Escreva um programa em que o usuário informe o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]".
+    
+    print("Digite o seu nome:")
+
+
+    var nome = readln()
+
+    print("Olá  $nome!")
+
+}
