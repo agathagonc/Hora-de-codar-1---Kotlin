34ed762062462785cae70f3073282cd7640f581c
Agatha Gonçalves <agathagoncalves002@gmail.com>
2025-03-17 21:22:58 -0300

    Add files via upload

---
+++ Quadrado.kt
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    // Quadrado
+
+    print("Digite o lado do quadrado: ")
+
+    val ladoQuadrado = readLine()!!.toDouble()
+
+    val areaQuadrado = ladoQuadrado * ladoQuadrado
+
+    println("Área do Quadrado: $areaQuadrado")
+
+}
